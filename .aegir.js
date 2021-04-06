041de46f85a5e96661b2732ea3316b73b136aa3a
achingbrain <alex@achingbrain.net>
2021-04-06 11:12:16 +0100

    chore: update build config

--- .aegir.js
+++ .aegir.js
@@ -1,4 +1,33 @@
-module.exports = {
-    bundlesize: { maxSize: '170kB' }
+'use strict'
+
+/** @type {import('aegir').Options["build"]["config"]} */
+const esbuild = {
+  plugins: [
+    {
+      name: 'node built ins',
+      setup (build) {
+        build.onResolve({ filter: /^stream$/ }, () => {
+          return { path: require.resolve('readable-stream') }
+        })
+        build.onResolve({ filter: /^crypto$/ }, () => {
+          return { path: require.resolve('crypto-browserify') }
+        })
+      }
+    }
+  ]
 }
-  
+
+/** @type {import('aegir').PartialOptions} */
+module.exports = {
+  test: {
+    browser: {
+      config: {
+        buildConfig: esbuild
+      }
+    }
+  },
+  build: {
+    bundlesizeMax: '457KB',
+    config: esbuild
+  }
+}
