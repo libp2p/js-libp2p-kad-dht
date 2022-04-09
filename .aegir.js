a851302ee3c2b1c7de1db4126cf08370b5003b04
achingbrain <alex@achingbrain.net>
2022-04-09 14:24:08 +0100

    chore: update aegir config

--- .aegir.js
+++ .aegir.js
@@ -1,29 +1,7 @@
 
-/** @type {import('aegir').Options["build"]["config"]} */
-const esbuild = {
-  plugins: [
-    {
-      name: 'node built ins',
-      setup (build) {
-        build.onResolve({ filter: /^stream$/ }, () => {
-          return { path: require.resolve('readable-stream') }
-        })
-      }
-    }
-  ]
-}
-
 /** @type {import('aegir').PartialOptions} */
 export default {
-  test: {
-    browser: {
-      config: {
-        buildConfig: esbuild
-      }
-    }
-  },
   build: {
-    bundlesizeMax: '300KB',
-    config: esbuild
+    bundlesizeMax: '300KB'
   }
 }
