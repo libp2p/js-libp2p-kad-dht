6b01a7106bf3dba0b61ca03d846293dab9298fca
zeroxbt <original.swazi@protonmail.com>
2022-09-07 19:16:26 +0200

    yield only FINAL_PEER events

--- src/peer-routing/index.ts
+++ src/peer-routing/index.ts
@@ -235,8 +235,6 @@ export class PeerRouting implements Initializable {
     }
 
     for await (const event of this.queryManager.run(key, tablePeers, getCloserPeersQuery, options)) {
-      yield event
-
       if (event.name === 'PEER_RESPONSE') {
         await Promise.all(event.closer.map(async peerData => await peers.add(peerData.id)))
       }
